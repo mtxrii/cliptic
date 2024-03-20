66de1fd4210403492fec7e49129258c964769087
Enrique <edavalos@ucsc.edu>
2024-03-20 00:24:42 -0700

    Added getRandomHash() to utils.js

--- cliptic-frontend/utils.js
+++ cliptic-frontend/utils.js
@@ -1,7 +1,15 @@
+import { customAlphabet } from "nanoid";
+
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
 export default function formatLog(logStr) {
     let date = new Date().toISOString();
     date = date.replaceAll('-', '/')
                .replace('T', ' ')
                .replace('Z', '');
     return '[' + date + '] ' + logStr;
+}
+
+export default function getRandomHash(length) {
+    return customAlphabet(characters, length);
 }
