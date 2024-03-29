84646ff0a4e6f3c2a5f6a255850583aa7f8d7200
Enrique <edavalos@ucsc.edu>
2024-03-28 23:24:00 -0700

    Removed mongodb sample code from mongodb.js

--- cliptic-backend/mongodb.js
+++ cliptic-backend/mongodb.js
@@ -1,34 +1,3 @@
-// read sample from: https://www.mongodb.com/docs/drivers/node/current/quick-start/connect-to-mongodb/
-const { MongoClient } = require("mongodb");
-// Replace the uri string with your connection string.
-const uri = "<connection string uri>";
-const client = new MongoClient(uri);
-async function run() {
-  try {
-    const database = client.db('sample_mflix');
-    const movies = database.collection('movies');
-    // Query for a movie that has the title 'Back to the Future'connect to mongodb
-    const query = { title: 'Back to the Future' };
-    const movie = await movies.findOne(query);
-    console.log(movie);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
-
-// write sample from: https://www.mongodb.com/docs/drivers/node/current/fundamentals/crud/write-operations/insert/
-const myDB = client.db("myDB");
-const myColl = myDB.collection("pizzaMenu");
-
-const doc = { name: "Neapolitan pizza", shape: "round" };
-const result = await myColl.insertOne(doc);
-console.log(
-   `A document was inserted with the _id: ${result.insertedId}`,
-);
-
 export const saveLink = async (db, url) => {
   const linkCollection = db.collection("link");
 
