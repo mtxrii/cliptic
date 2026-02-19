a4605dbb0dd5aec2ffe9827734dcca5f43818346
Enrique <edavalos@ucsc.edu>
2026-02-18 19:41:40 -0800

    Added JsonInclude annotation to PostUrlRequest

--- cliptic-backend-v2/src/main/java/com/mtxrii/cliptic/clipticbackend/api/model/request/PostUrlRequest.java
+++ cliptic-backend-v2/src/main/java/com/mtxrii/cliptic/clipticbackend/api/model/request/PostUrlRequest.java
@@ -1,10 +1,12 @@
 package com.mtxrii.cliptic.clipticbackend.api.model.request;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class PostUrlRequest {
     private final String originalUrl;
-    private final String customAlias;
+    private final String alias;
     private final String createdBy;
 }
