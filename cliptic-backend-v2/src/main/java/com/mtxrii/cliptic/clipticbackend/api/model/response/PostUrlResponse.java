2c29c8c486ff5d8cb3f6097c15f1e1270444e949
Enrique <edavalos@ucsc.edu>
2026-02-19 21:10:12 -0800

    Added @Getter annotation to PostUrlResponse

--- cliptic-backend-v2/src/main/java/com/mtxrii/cliptic/clipticbackend/api/model/response/PostUrlResponse.java
+++ cliptic-backend-v2/src/main/java/com/mtxrii/cliptic/clipticbackend/api/model/response/PostUrlResponse.java
@@ -1,5 +1,8 @@
 package com.mtxrii.cliptic.clipticbackend.api.model.response;
 
+import lombok.Getter;
+
+@Getter
 public class PostUrlResponse extends Response {
     private final String shortenedUrl;
 
