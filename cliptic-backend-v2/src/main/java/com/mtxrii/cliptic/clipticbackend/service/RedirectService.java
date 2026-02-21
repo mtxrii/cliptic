7a0312f7d79da7a97c5738f20c38f60cb8568680
Enrique <edavalos@ucsc.edu>
2026-02-21 00:49:53 -0800

    Added RedirectService

---
+++ cliptic-backend-v2/src/main/java/com/mtxrii/cliptic/clipticbackend/service/RedirectService.java
@@ -0,0 +1,21 @@
+package com.mtxrii.cliptic.clipticbackend.service;
+
+import com.mtxrii.cliptic.clipticbackend.ClipticConst;
+import com.mtxrii.cliptic.clipticbackend.db.LinkRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedirectService {
+    private final LinkRepository linkRepository;
+
+    public RedirectService(LinkRepository linkRepository) {
+        this.linkRepository = linkRepository;
+    }
+
+    public String redirect(String alias) {
+        if (this.linkRepository.findByAlias(alias).isEmpty()) {
+            return null;
+        }
+        return ClipticConst.REDIRECT_BASE_URL + this.linkRepository.findByAlias(alias).get().getAlias();
+    }
+}
