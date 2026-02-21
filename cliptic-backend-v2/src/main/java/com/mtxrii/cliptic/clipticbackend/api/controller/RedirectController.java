5d4fbe585fbbf42e0bcbd7ee236261a4986990c0
Enrique <edavalos@ucsc.edu>
2026-02-21 00:54:32 -0800

    Modified RedirectController to use RedirectService

--- cliptic-backend-v2/src/main/java/com/mtxrii/cliptic/clipticbackend/api/controller/RedirectController.java
+++ cliptic-backend-v2/src/main/java/com/mtxrii/cliptic/clipticbackend/api/controller/RedirectController.java
@@ -1,5 +1,6 @@
 package com.mtxrii.cliptic.clipticbackend.api.controller;
 
+import com.mtxrii.cliptic.clipticbackend.service.RedirectService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,12 +9,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RedirectController {
+    private final RedirectService redirectService;
+
+    public RedirectController(RedirectService redirectService) {
+        this.redirectService = redirectService;
+    }
 
     @GetMapping("/{alias}")
     public ResponseEntity<Void> redirectToUrl(
             @PathVariable String alias
     ) {
-        String redirectUrl = "https://web.postman.co/"; // This is temporary
+        if (alias == null) {
+            return ResponseEntity.status(404).build();
+        }
+        String redirectUrl = this.redirectService.redirect(alias);
+        if (redirectUrl == null) {
+            return ResponseEntity.status(404).build();
+        }
         return ResponseEntity
                 .status(302)
                 .header(HttpHeaders.LOCATION, redirectUrl)
