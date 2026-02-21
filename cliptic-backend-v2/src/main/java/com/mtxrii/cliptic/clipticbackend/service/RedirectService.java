072d0d66d6c51080fba6809f454b7d4be7ac29e8
Enrique <edavalos@ucsc.edu>
2026-02-21 01:04:22 -0800

    Added local cache to RedirectService

--- cliptic-backend-v2/src/main/java/com/mtxrii/cliptic/clipticbackend/service/RedirectService.java
+++ cliptic-backend-v2/src/main/java/com/mtxrii/cliptic/clipticbackend/service/RedirectService.java
@@ -3,8 +3,13 @@ package com.mtxrii.cliptic.clipticbackend.service;
 import com.mtxrii.cliptic.clipticbackend.db.LinkRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class RedirectService {
+    private static final Map<String, String> REDIRECT_CACHE = new HashMap<>();
+
     private final LinkRepository linkRepository;
 
     public RedirectService(LinkRepository linkRepository) {
@@ -12,9 +17,16 @@ public class RedirectService {
     }
 
     public String redirect(String alias) {
+        if (REDIRECT_CACHE.containsKey(alias)) {
+            return REDIRECT_CACHE.get(alias);
+        }
+
         if (this.linkRepository.findByAlias(alias).isEmpty()) {
             return null;
         }
-        return this.linkRepository.findByAlias(alias).get().getOriginalUrl();
+
+        String redirectUrl = this.linkRepository.findByAlias(alias).get().getOriginalUrl();
+        REDIRECT_CACHE.put(alias, redirectUrl);
+        return redirectUrl;
     }
 }
