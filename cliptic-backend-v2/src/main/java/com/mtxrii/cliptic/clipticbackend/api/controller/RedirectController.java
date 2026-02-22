dc4cbf30b141fc3edae8c42c1ecdd3403f9a9118
Enrique <edavalos@ucsc.edu>
2026-02-22 03:04:06 -0800

    Made return value Optional<> of redirect() in RedirectService

--- cliptic-backend-v2/src/main/java/com/mtxrii/cliptic/clipticbackend/api/controller/RedirectController.java
+++ cliptic-backend-v2/src/main/java/com/mtxrii/cliptic/clipticbackend/api/controller/RedirectController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 public class RedirectController {
     private final RedirectService redirectService;
@@ -22,13 +24,13 @@ public class RedirectController {
         if (alias == null) {
             return ResponseEntity.status(404).build();
         }
-        String redirectUrl = this.redirectService.redirect(alias);
-        if (redirectUrl == null) {
+        Optional<String> redirectUrl = this.redirectService.redirect(alias);
+        if (redirectUrl.isEmpty()) {
             return ResponseEntity.status(404).build();
         }
         return ResponseEntity
                 .status(302)
-                .header(HttpHeaders.LOCATION, redirectUrl)
+                .header(HttpHeaders.LOCATION, redirectUrl.get())
                 .build();
     }
 }
--- cliptic-backend-v2/src/main/java/com/mtxrii/cliptic/clipticbackend/service/RedirectService.java
+++ cliptic-backend-v2/src/main/java/com/mtxrii/cliptic/clipticbackend/service/RedirectService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class RedirectService {
@@ -16,18 +17,18 @@ public class RedirectService {
         this.linkRepository = linkRepository;
     }
 
-    public String redirect(String alias) {
+    public Optional<String> redirect(String alias) {
         if (REDIRECT_CACHE.containsKey(alias)) {
-            return REDIRECT_CACHE.get(alias);
+            return Optional.of(REDIRECT_CACHE.get(alias));
         }
 
         if (this.linkRepository.findByAlias(alias).isEmpty()) {
-            return null;
+            return Optional.empty();
         }
 
         String redirectUrl = this.linkRepository.findByAlias(alias).get().getOriginalUrl();
         REDIRECT_CACHE.put(alias, redirectUrl);
-        return redirectUrl;
+        return Optional.of(redirectUrl);
     }
 
     public static void addToCache(String alias, String redirectUrl) {
