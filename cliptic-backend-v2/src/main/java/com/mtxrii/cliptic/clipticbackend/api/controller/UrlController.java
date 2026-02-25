3d38297f8f1f104ba658620851f5acb6299c2991
Enrique <edavalos@ucsc.edu>
2026-02-24 23:58:19 -0800

    Added HTTP method logging to runIfAuthenticated() in UrlController

--- cliptic-backend-v2/src/main/java/com/mtxrii/cliptic/clipticbackend/api/controller/UrlController.java
+++ cliptic-backend-v2/src/main/java/com/mtxrii/cliptic/clipticbackend/api/controller/UrlController.java
@@ -8,6 +8,7 @@ import com.mtxrii.cliptic.clipticbackend.service.UrlService;
 import com.mtxrii.cliptic.clipticbackend.util.GenericUtil;
 import io.github.cdimascio.dotenv.Dotenv;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,6 +37,7 @@ public class UrlController {
             @RequestBody PostUrlRequest requestBody
     ) {
         Response response = this.runIfAuthenticated(
+                HttpMethod.POST,
                 authHeader,
                 () -> this.urlService.postUrl(requestBody)
         );
@@ -48,6 +50,7 @@ public class UrlController {
             @RequestParam(name = ClipticConst.ALIAS_REQUEST_PARAM, required = false) String alias
     ) {
         Response response = this.runIfAuthenticated(
+                HttpMethod.GET,
                 authHeader,
                 () -> this.urlService.getUrl(alias)
         );
@@ -61,26 +64,28 @@ public class UrlController {
             @RequestParam(name = ClipticConst.OWNER_REQUEST_PARAM, defaultValue = ClipticConst.NONE_AUTH_HEADER) String owner
     ) {
         Response response = this.runIfAuthenticated(
+                HttpMethod.DELETE,
                 authHeader,
                 () -> this.urlService.deleteUrl(alias, owner)
         );
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }
 
-    private Response runIfAuthenticated(String authHeader, Callable<Response> callable) {
+    private Response runIfAuthenticated(HttpMethod method, String authHeader, Callable<Response> callable) {
         if (authHeader == null || !authHeader.startsWith(ClipticConst.BEARER_AUTH_HEADER_PREFIX)) {
-            log.info("Error on {}: Missing Bearer token", ClipticConst.MAPPING_URL_CONTROLLER);
+            log.info("Error on {} {}: Missing Bearer token", method.name(), ClipticConst.MAPPING_URL_CONTROLLER);
             return new ErrorResponse(401, "Unauthenticated. Missing or invalid token");
         }
 
         String token = authHeader.substring(ClipticConst.BEARER_AUTH_HEADER_PREFIX.length());
         String expected = this.dotenv.get(ClipticConst.BEARER_TOKEN_ENV_VAR_KEY);
         if (!GenericUtil.equals(token, expected)) {
-            log.info("Error on {}: Invalid Bearer token", ClipticConst.MAPPING_URL_CONTROLLER);
+            log.info("Error on {} {}: Invalid Bearer token", method.name(), ClipticConst.MAPPING_URL_CONTROLLER);
             return new ErrorResponse(401, "Access denied. Invalid token");
         }
 
         try {
+            log.info("Successfully authenticated {} {}", method.name(), ClipticConst.MAPPING_URL_CONTROLLER);
             return callable.call();
         } catch (Exception e) {
             log.error("Error occurred while processing request", e);
